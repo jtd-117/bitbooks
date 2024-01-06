2eb6d2efce87e519c78d8549f345630d04df8943
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-01-06 13:34:36 +1100

    Add CMPValues enum

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
  * @file script.js
  * @description A file for implementing the BitBooks logic
  */
+
+/**
+ * @description An enumeration for comparing 2 values.
+ */
+const CMPValues = Object.freeze({
+    LESS: -1,
+    EQUAL: 0,
+    GREATER: 1,
+});
